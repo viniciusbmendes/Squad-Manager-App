f8fc93a6f5c69ba00b5a6da312c6cc07653f111c
viniciusbmendes <vinicius.bmendes@live.com>
2024-07-07 13:27:02 -0300

    chore: Post requisition and Refactor add_player_form component to use select dropdown for player class selection

--- app/frontend/src/components/add_player_form.tsx
+++ app/frontend/src/components/add_player_form.tsx
@@ -1,16 +1,81 @@
 import { useState } from 'react';
+import Swal from 'sweetalert2';
 
 interface MyFormProps {
-  onSubmit: (data: { nickname: string; class: string }) => void;
+	onSubmit: (data: { nickname: string; class: string }) => void;
 }
 
-const AddPlayerForm: React.FC<MyFormProps> = ({ onSubmit }) => {
-	const [nickname, setNickname] = useState('');
-	const [playerClass, setPlayerClass] = useState('');
+const classes = [
+	'Warrior',
+	'Paladin',
+	'Hunter',
+	'Rogue',
+	'Priest',
+	'Death Knight',
+	'Shaman',
+	'Mage',
+	'Warlock',
+	'Monk',
+	'Druid',
+	'Demon Hunter',
+];
 
-	const handleSubmit = (e: React.FormEvent) => {
+const AddPlayerForm: React.FC<MyFormProps> = ({ onSubmit }) => {
+	const [player, setPlayer] = useState({
+		nickname: '',
+		class: classes[0],
+	});
+
+	const handleChange = (
+		e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+	) => {
+		setPlayer({
+			...player,
+			[e.target.name]: e.target.value,
+		});
+	};
+
+	const handleSubmit = async (e: React.FormEvent) => {
 		e.preventDefault();
-		onSubmit({ nickname, class: playerClass });
+		onSubmit({ nickname: player.nickname, class: player.class });
+
+		try {
+			const response = await fetch('http://localhost:3001/players', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify(player),
+			});
+
+			if (response.status === 409) {
+				Swal.fire({
+					icon: "warning",
+					title: "Oops...",
+					text: "Ja existe um player com esse nome!",
+				});
+			} else if (response.ok) {
+				Swal.fire({
+					icon: "success",
+					title: "Player cadastrado com sucesso!",
+					showConfirmButton: true,
+					timer: 4000,
+				})
+			} else {
+				Swal.fire({
+					icon: "warning",
+					title: "Oops...",
+					text: "Resposta inesperada do servidor! Tente novamente!",
+				});
+			}
+		} catch (error) {
+			console.error('Error:', error);
+			Swal.fire({
+				icon: "error",
+				title: "Oops...",
+				text: "Algo deu errado! Tente novamente!",
+			});
+		}
 	};
 
 	return (
@@ -19,21 +84,25 @@ const AddPlayerForm: React.FC<MyFormProps> = ({ onSubmit }) => {
 				Nick:
 				<input
 					type='text'
-					value={nickname}
-					onChange={(e) => setNickname(e.target.value)}
+					name='nickname'
+					value={player.nickname}
+					onChange={handleChange}
+					required
 				/>
 			</label>
 			<br />
 			<label>
 				Class:
-				<input
-					type='text'
-					value={playerClass}
-					onChange={(e) => setPlayerClass(e.target.value)}
-				/>
+				<select name='class' value={player.class} onChange={handleChange}>
+					{classes.map((c) => (
+						<option key={c} value={c}>
+							{c}
+						</option>
+					))}
+				</select>
 			</label>
 			<br />
-			<button type='submit'>Submit</button>
+			<button type='submit'>Cadastrar</button>
 		</form>
 	);
 };
